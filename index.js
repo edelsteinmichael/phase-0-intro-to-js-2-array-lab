48809b8c498426f440ba23735478ddfa5098d298
Michael <edelsteinmichael23@gmail.com>
2021-12-20 15:07:09 -0500

    Done.

--- index.js
+++ index.js
@@ -2,17 +2,39 @@
 const cats = ['Milo', 'Otis', 'Garfield']
  function destructivelyAppendCat(name) {
     cats.push(name)
- }
+}
  function destructivelyPrependCat(name) {
      cats.unshift(name)
- }
+}
  function destructivelyRemoveLastCat() {
      cats.pop()
- }
+}
  function destructivelyRemoveFirstCat() {
      cats.shift()
- }
- let appendCat = [...cats]
+}
+ function appendCat(name) {
+    let appendCopyOfCats = [...cats]
+     appendCopyOfCats.push(name)
+    return appendCopyOfCats
+}
+function prependCat(name) {
+    let prependCopyOfCats = [...cats]
+     prependCopyOfCats.unshift(name)
+    return prependCopyOfCats
+}
+function removeLastCat() {
+    let lastCat = [...cats]
+    lastCat.pop()
+    return lastCat
+}
+function removeFirstCat() {
+    let firstCat = [...cats]
+    firstCat.shift()
+    return firstCat
+}
+
+
+
  
 
 
